81eb73e9cdb06ad828bf1cd7c983c7a2246badb9
Emily Taylor <emilytaylor23@outlook.com>
2018-09-09 16:40:20 +1000

    add react-router and start integrating Simon

---
+++ src/Home.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import {
+  Link
+} from 'react-router-dom'
+
+import './Home.css';
+
+import emilyPhoto from './images/emilyPhoto.JPG';
+import calculator from './images/calculator.jpg';
+import pomodoroClock from './images/pomodoroClock.jpg';
+import quoteMachine from './images/quoteMachine.jpg';
+import twitchTV from './images/twitchTV.jpg';
+import weatherApp from './images/weatherApp.jpg';
+import wikipediaViewer from './images/wikipediaViewer.jpg';
+import ticTacToe from './images/ticTacToe.jpg';
+import simonGame from './images/simonGame.jpg';
+import recipeShelf from './images/recipeShelf.jpg';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className='Home'>
+        <div className='Paper paper-effect'>
+          <div className='Content'>
+            <div className='About-Me'>
+              <div>
+                <h1 className='initial-heading'>Emily Jane Taylor</h1>
+                <p>A self-taught front-end developer with a background in primary education and editing. </p>
+                <p className='ul-heading'>Has completed:</p>
+                <ul>
+                  <li><b>FreeCodeCamp's Front End Development Certification</b></li>
+                  <li><b>Udemy's React 16 - A Complete Guide Certification</b> (React components)</li>
+                </ul>
+                <p>Proficient at using Javascript, React, CSS, Bootstrap and HTML, with experience in Git.</p>
+                <p>Hard working and enthusiastic with strong attention to detail and a desire to learn.</p>
+
+              </div>
+              <div className='emily-photo-div'>
+                <img className='emily-photo' src={emilyPhoto} alt='Emily Taylor' />
+                <div className='links'>
+                  <a href="http://www.linkedin.com/in/emilytaylor23" target="_blank"><i className='fa fa-linkedin fa-lg'></i></a>
+                  <a href="http://www.freecodecamp.com/whylime23" target="_blank"><i className='fa fa-free-code-camp fa-lg'></i></a>
+                  <a href="http://www.github.com/whylime23" target="_blank"><i className='fa fa-github fa-lg'></i></a>
+                </div>
+              </div>
+
+            </div>
+            <div>
+              <h1 className='portfolio-heading'>Portfolio</h1>
+
+              <h2>Independent Projects</h2>
+              <div className='Portfolio'>
+                <a href="http://www.recipeshelf.com.au" target="blank"><img className='screenshot-important' src={recipeShelf} alt='Recipe Shelf' /></a>
+                <div className='screenshot-desc'>
+                  <p className='caption'>Working collaboratively to rebuild the existing website in React.</p>
+                </div>
+              </div>
+
+              <h2>FreeCodeCamp Projects</h2>
+              <div className='Portfolio'>
+                <Link to="/simon" className="screenshot-linked"><img className='screenshot' src={simonGame} alt='Simon Game' /></Link>
+                {/* <a href="https://github.com/whylime23/simon-game" target="_blank" className="screenshot-linked"><img className='screenshot' src={simonGame} alt='Simon Game' /></a> */}
+                <a href="https://github.com/whylime23/tic-tac-toe" target="_blank" className="screenshot-linked"><img className='screenshot' src={ticTacToe} alt='Tic-tac-toe' /></a>
+                <a href="https://github.com/whylime23/pomodoro-clock" target="_blank" className="screenshot-linked"><img className='screenshot' src={pomodoroClock} alt='Pomodoro Clock' /></a>
+                <a href="https://codepen.io/whylime23/full/NYwgMy/" target="_blank" className="screenshot-linked"><img className='screenshot' src={calculator} alt='Javascript Calculator' /></a>
+                <a href="https://codepen.io/whylime23/full/vJPJWm/" target="_blank" className="screenshot-linked"><img className='screenshot' src={twitchTV} alt='TwitchTV Streamer' /></a>
+                <a href="https://codepen.io/whylime23/full/MvPWMK/" target="_blank" className="screenshot-linked"><img className='screenshot' src={wikipediaViewer} alt='Wikipedia Viewer' /></a>
+                <a href="https://codepen.io/whylime23/full/OjEPGO/" target="_blank" className="screenshot-linked"><img className='screenshot' src={weatherApp} alt='Local Weather App' /></a>
+                <a href="https://codepen.io/whylime23/full/PKJqbG/" target="_blank" className="screenshot-linked"><img className='screenshot' src={quoteMachine} alt='Random Quote Machine' /></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='Footer'>
+          Designed and coded by Emily Taylor using Atom, React and Chrome dev tools. Background image by Victor Santos at Pexels.
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/Simon/Simon.js
@@ -0,0 +1,298 @@
+/* eslint jsx-a11y/media-has-caption: 0 */
+import React, { Component } from 'react';
+
+import './Simon.css';
+import greenSound from './sounds/simonSound1.mp3';
+import redSound from './sounds/simonSound2.mp3';
+import yellowSound from './sounds/simonSound3.mp3';
+import blueSound from './sounds/simonSound4.mp3';
+
+const BUTTON_LIT_LENGTH = 500;
+const BUTTON_CYCLE_LENGTH = BUTTON_LIT_LENGTH * 2;
+
+class Simon extends Component {
+  state = {
+    isOn: false,
+    isStrict: false,
+    clickedColour: '',
+    activeQuadrant: '',
+    playerTurn: 'computer',
+    error: false,
+    win: false,
+    computerSequence: [],
+    userSequence: []
+  }
+
+  generateQuadrant = () => {
+    switch (Math.floor(Math.random() * (4) + 1)) {
+      case 1:
+        return 'green';
+      case 2:
+        return 'red';
+      case 3:
+        return 'yellow';
+      case 4:
+        return 'blue';
+    }
+  }
+
+  playSound = (colour) => {
+    switch(colour) {
+      case 'green':
+        this.green.play()
+        break;
+      case 'red':
+        this.red.play()
+        break;
+      case 'yellow':
+        this.yellow.play()
+        break;
+      case 'blue':
+        this.blue.play()
+        break;
+    }
+  }
+
+  buttonLit = (colour) => {
+    const { activeQuadrant, clickedColour } = this.state;
+    return activeQuadrant === colour || clickedColour === colour ? `${colour}-button-lit` : '';
+  }
+
+  playButtonsEnable = () => {
+    return this.state.playerTurn === 'computer';
+  }
+
+  lightQuadrant = (colour) => {
+    this.setState({ clickedColour: colour }, () => {
+      setTimeout(() => {
+        this.setState({ clickedColour: '' });
+      }, BUTTON_LIT_LENGTH)
+    }
+    )
+  }
+
+  computerTurn = () => {
+    const { computerSequence } = this.state;
+    const randomColour = this.generateQuadrant();
+    // catches error from strict reset
+    this.setState({
+      error: false
+    });
+    // add computer colour to computer sequence
+    computerSequence.push(randomColour);
+    // run computer sequence
+    this.lightComputerSequence();
+  }
+
+  computerTurnWithTimeout = () => {
+    setTimeout(this.computerTurn, BUTTON_CYCLE_LENGTH);
+  };
+
+  lightComputerSequence = () => {
+    const { computerSequence } = this.state;
+    console.log(computerSequence); // included for testing
+    for (let i = 0; i < computerSequence.length; i++) {
+      setTimeout(() => {
+        this.playSound(computerSequence[i])
+        this.setState({ activeQuadrant: computerSequence[i] }, () => {
+          setTimeout(() => {
+            this.setState({ activeQuadrant: '', playerTurn: 'user'});
+          }, BUTTON_LIT_LENGTH)
+        })
+      }, (i + 1) * BUTTON_CYCLE_LENGTH)
+    }
+  }
+
+  lightWinSequence = () => {
+    const winSequence = ['green', 'red', 'blue', 'yellow', 'green', 'red', 'blue', 'yellow'];
+    for (let i = 0; i < winSequence.length; i++) {
+      setTimeout(() => {
+        this.setState({ activeQuadrant: winSequence[i] }, () => {
+          setTimeout(() => {
+            this.setState({ activeQuadrant: '' });
+          }, 250)
+        })
+      }, (i + 1) * 500)
+    }
+
+  }
+
+  checkUserTurn = () => {
+    const { userSequence, computerSequence, isStrict } = this.state;
+    for (let i = 0; i < userSequence.length; i++) {
+      if (userSequence[i] !== computerSequence[i]) {
+        if (isStrict) {
+          this.setState({
+            playerTurn: 'computer',
+            error: true,
+            computerSequence: [],
+            userSequence: []
+          }, this.computerTurnWithTimeout);
+        }
+        this.setState({
+          userSequence: [],
+          error: true,
+          playerTurn: 'user'
+        }, this.lightComputerSequence);
+        return;
+      }
+    }
+
+    if (userSequence.length !== computerSequence.length) {
+      return;
+    }
+
+    if (userSequence.length === 20) {
+      this.lightWinSequence();
+      this.setState({
+        win: true
+      }, this.onWinWithTimeout);
+      return;
+    }
+
+    this.setState({
+      playerTurn: 'computer',
+      userSequence: [],
+    }, this.computerTurnWithTimeout);
+  }
+
+  onWin = () => {
+    this.setState({
+      clickedColour: '',
+      activeQuadrant: '',
+      playerTurn: 'computer',
+      win: false,
+      computerSequence: [],
+      userSequence: []
+    }, this.computerTurn);
+  }
+
+  onWinWithTimeout = () => {
+    setTimeout(this.onWin, 5000)
+  }
+
+  handleStart = () => {
+    this.computerTurn();
+  }
+
+  handleOnToggle = () => {
+    if (this.state.isOn === true) {
+      this.setState({
+        isOn: false,
+        isStrict: false,
+        playerTurn: 'computer',
+        computerSequence: [],
+        userSequence: []
+      });
+    } else if (this.state.isOn === false) {
+      this.setState({
+        isOn: true
+      });
+    }
+  }
+
+  handleStrictMode = () => {
+    this.setState({
+      isStrict: !this.state.isStrict
+    });
+  }
+
+  handleQuadrantClick = (colour) => {
+    this.setState({
+      error: false
+    });
+    this.lightQuadrant(colour);
+    this.playSound(colour);
+    this.state.userSequence.push(colour);
+    this.checkUserTurn();
+  }
+
+  renderCountDisplay = () => {
+    const { computerSequence, isOn, error, win } = this.state;
+    if (!isOn) {
+      return '--';
+    } else if (error) {
+      return '! !';
+    } else if (win) {
+      return '😄'
+    } else {
+      return computerSequence.length;
+    }
+  }
+
+  render () {
+    const { isOn, isStrict } = this.state;
+
+    return (
+      <div className='Simon'>
+        <div className='Console'>
+          <div className='Controls'>
+            <h1 className='Simon-title'>SIMON</h1>
+            <div className='Control-bulk'>
+              <div className='Control-buttons'>
+                <span className='Count-display'>{this.renderCountDisplay()}</span>
+                <span><p className='button-label'>COUNT</p></span>
+              </div>
+              <div className='Control-buttons'>
+                <button className='Start-button indv-item' onClick={this.handleStart} disabled={!isOn}></button>
+                <span><p className='button-label'>START</p></span>
+              </div>
+              <div className='Control-buttons'>
+                <span className={`Strict-light ${!isStrict ? 'strict-light-off' : 'strict-light-on'}`}></span>
+                <button className='Strict-button' disabled={!isOn} onClick={this.handleStrictMode}></button>
+                <span><p className='button-label'>STRICT</p></span>
+              </div>
+            </div>
+            <div className='On-toggle'>
+              <p>ON</p>
+              <button className={`toggle-button `} onClick={this.handleOnToggle}>
+                <div className='toggle-icon'>
+                  <i className={`fa fa-toggle-on fa-2x ${isOn ? 'rotate-icon' : ''}`}></i>
+                </div>
+              </button>
+              <p>OFF</p>
+            </div>
+          </div>
+
+          <div className='Row'>
+            <div className='Buttons Green border-top border-left border-right'>
+              <audio ref={(element) => { this.green = element; }}>
+                <source src={greenSound} type='audio/mpeg'/>
+              </audio>
+              <button className={`1 green-button play-buttons ${this.buttonLit('green')}`} onClick={() => this.handleQuadrantClick('green')} disabled={this.playButtonsEnable()}></button>
+            </div>
+            <div className='Buttons Red border-top border-right'>
+              <audio ref={(element) => { this.red = element; }}>
+                <source src={redSound} type='audio/mpeg'/>
+              </audio>
+              <button className={`2 red-button play-buttons ${this.buttonLit('red')}`} onClick={() => this.handleQuadrantClick('red')} disabled={this.playButtonsEnable()}></button>
+            </div>
+          </div>
+          <div className='Row'>
+            <div className='Buttons Yellow border-top border-bottom border-left border-right'>
+              <audio ref={(element) => { this.yellow = element; }}> ? true : false
+                <source src={yellowSound} type='audio/mpeg'/>
+              </audio>
+              <button className={`3 yellow-button play-buttons ${this.buttonLit('yellow')}`} onClick={() => this.handleQuadrantClick('yellow')} disabled={this.playButtonsEnable()}></button>
+            </div>
+            <div className='Buttons Blue border-top border-bottom border-right'>
+              <audio ref={(element) => { this.blue = element; }}>
+                <source src={blueSound} type='audio/mpeg'/>
+              </audio>
+              <button className={`4 blue-button play-buttons ${this.buttonLit('blue')}`} onClick={() => this.handleQuadrantClick('blue')} disabled={this.playButtonsEnable()}></button>
+            </div>
+          </div>
+        </div>
+        <div className='Footer'>
+        Designed and coded by &nbsp;<a
+            className='emily-link'
+            href='https://www.freecodecamp.com/whylime23'
+            target='_blank'
+            rel='noopener noreferrer'>Emily Taylor</a>&nbsp; using Atom, React and Chrome dev tools. Background image by rawpixel.com from Pexels.
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Simon;
